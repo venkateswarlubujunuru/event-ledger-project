0d9f2f5baa0207cf3b0605f37787fd4de1f260f3
venkateswarlubujunuru <venkateswarlubujunuru@gmail.com>
2026-07-09 14:56:19 +0530

    Initial project setup

---
+++ event-gateway/src/main/java/com/gatewayservice/EventGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.gatewayservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventGatewayApplication.class, args);
+	}
+
+}
